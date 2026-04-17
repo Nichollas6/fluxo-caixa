da2a95687369bfa803a0fd8d7077b1029eee6404
Nichollas6 <nichollaskylssen6@gmail.com>
2026-04-17 15:06:16 -0300

    add gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
 const app = express();
 
 // 🔥 middlewares
