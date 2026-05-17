c29545677ce70b3c2f9a0dc92737c52a8ea714e6
Nichollas6 <nichollaskylssen6@gmail.com>
2026-05-17 09:45:24 -0300

    Resolvendo token8

--- vite-project/src/services/api.js
+++ vite-project/src/services/api.js
@@ -1,34 +1,28 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://fluxo-caixa-back.onrender.com"
+  baseURL: "https://fluxo-caixa-back.onrender.com"
 });
 
-// envia token automaticamente
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
+  console.log("TOKEN ENVIADO:", token);
+
   if (token) {
-    config.headers = config.headers || {};
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`
+    };
   }
 
   return config;
 });
 
-// trata token expirado
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      localStorage.clear();
-
-      if (window.location.pathname !== "/login") {
-        window.location.href = "/login";
-      }
-    }
+    console.log("ERRO API:", error.response?.data);
 
     return Promise.reject(error);
   }
